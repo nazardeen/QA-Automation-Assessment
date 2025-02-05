e08bbc1a05c3bc0eb1021033083951d5af1555a4
Abdul Nazardeen <rahnazardeen@gmail.com>
2025-02-05 14:34:14 +0530

    upload all files

--- api-automation/RestfulBookerTests/obj/Debug/net8.0/RestfulBookerTests.AssemblyInfo.cs
+++ api-automation/RestfulBookerTests/obj/Debug/net8.0/RestfulBookerTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestfulBookerTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa0b01592fe49f123b2c388b0435c53d9abb765b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd594a58f4604488fded8b81dbb9a8bf3bfc76ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestfulBookerTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestfulBookerTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
